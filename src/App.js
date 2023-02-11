7dce5abfc0ac9b92f0feead1e3633b7fd3b00068
mo7tr <mo7tr@gmail.com>
2023-02-11 16:33:35 +0100

    implementing usedispatch in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Form from "./src/components/Form";
-import PicCard from "./src/components/PicCard";
+import Form from "./components/Form";
+import PicCard from "./components/PicCard";
+import { useDispatch } from "react-redux";
+import { setPicturesData } from "./store/pictures/pictures.slice";
 
 const App = () => {
-  const [picsData, setPicsData] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/pictures")
-      .then((res) => setPicsData(res.data));
+      .then((res) => dispatch(setPicturesData(res.data)));
   }, []);
 
   return (
     <>
       <h1>NFT Gallery</h1>
       <Form />
-      <div className="cards-container">
+      {/* <div className="cards-container">
         {picsData?.map((pic, index) => (
           <PicCard key={index} pic={pic} />
         ))}
-      </div>
+      </div> */}
     </>
   );
 };
